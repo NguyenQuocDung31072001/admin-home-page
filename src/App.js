19cccf0fc3284de1974700aedb78b36da8e01a4b
bifann317 <bifansuppercode317@gmail.com>
2021-05-07 19:46:39 +0700

    add material

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import {BrowserRouter as Router,Switch, Route, Redirect } from 'react-router-dom'
-import Home from './page/home'
-import LoginFunction from './page/loginFunction'
 import NotFound from './page/notFound'
+import ContainedButtons from './material-ui/myButton'
+import ContainedButtonsHome from './material-ui/myButtonHome'
 function App() {
   return (
     <div className="App">     
        <Router>        
            <Switch>
              <Route  path='/home' render={()=>{
-               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<Home/>:<Redirect to='/login'></Redirect>
+               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<ContainedButtonsHome/>:<Redirect to='/login'></Redirect>
              }}></Route>
               <Route exact path='/' render={()=>{
-               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<Home/>:<Redirect to='/login'></Redirect>
+               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<ContainedButtonsHome/>:<Redirect to='/login'></Redirect>
              }}></Route>
 
               <Route path='/login' render={()=>{
-               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<Redirect to='/home'></Redirect>:<LoginFunction/>
+               return (localStorage.getItem('email')!==null &&localStorage.getItem('user')!==null && localStorage.getItem('pass')!==null)?<Redirect to='/home'></Redirect>:<ContainedButtons/>
              }}></Route>
 
               <Route component={NotFound}></Route>
---
+++ src/material-ui/myButtonHome.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { useHistory} from 'react-router-dom'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ContainedButtonsHome() {
+  const classes = useStyles();
+  const history=useHistory()
+  const logout=()=>{   
+      localStorage.removeItem('email')      
+      localStorage.removeItem('user')
+      localStorage.removeItem('pass')      
+      history.replace('/login')       
+  }
+
+  return (
+    <div className={classes.root}>
+        <h1>welcome home page</h1>
+            <p>email : {localStorage.getItem('email')}</p>
+            <p>username: {localStorage.getItem('user')}</p>
+            <p>password: {localStorage.getItem('pass')}</p>
+      <Button variant="contained" color="primary" onClick={logout}>
+        logout
+      </Button>
+    </div>
+  );
+}
